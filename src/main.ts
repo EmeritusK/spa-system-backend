33f57d4f6dcfc6f1116657cfc07abc35031cbb87
EmeritusK <jparedes1270@gmail.com>
2025-08-11 23:43:24 -0500

    Habilitar CORS en la aplicación para permitir solicitudes desde diferentes orígenes.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('SPA System API')
     .setDescription('Documentación de la API para el sistema de SPA')
